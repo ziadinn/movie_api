cf5a5bb9675b7d445aecf82ba231f72fd4559502
jackalnom <jackalnom@gmail.com>
2023-04-17 21:01:29 -0700

    Raising error if env is missing and fixed pytest

--- src/database.py
+++ src/database.py
@@ -3,10 +3,17 @@ from src.datatypes import Character, Movie, Conversation, Line
 import os
 import io
 from supabase import Client, create_client
+import dotenv
 
 # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
+dotenv.load_dotenv()
 supabase_api_key = os.environ.get("SUPABASE_API_KEY")
 supabase_url = os.environ.get("SUPABASE_URL")
+
+if supabase_api_key is None or supabase_url is None:
+    raise Exception(
+        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
+    )
 
 supabase: Client = create_client(supabase_url, supabase_api_key)
 
